85176e7f89a25e5cf1fec4ed9d0897d33ffda444
shunji <syu_syunji1990@yahoo.co.jp>
2021-11-27 20:34:45 +0900

    Initial Commit

---
+++ MyPromotion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyPromotion
+//
+//  Created by 朱駿璽 on 2021/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyPromotion/MyPromotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPromotionApp.swift
+//  MyPromotion
+//
+//  Created by 朱駿璽 on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct MyPromotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
