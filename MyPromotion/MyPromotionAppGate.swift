edf3689715c1d2d53dd9aa1b1ff407f1bc2206ff
shunji0223 <syu_syunji1990@yahoo.co.jp>
2021-11-29 23:52:10 +0900

    registrationApi Async/Sync done

---
+++ MyPromotion/MyPromotionAppGate.swift
@@ -0,0 +1,37 @@
+//
+//  MyPromotionApp.swift
+//  MyPromotion
+//
+//  Created by 朱駿璽 on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct MyPromotionApp: App {
+    @Environment(\.scenePhase) var scenePhase
+            
+    private let msAuthState: MSAuthState = resolve()
+    
+    private let msAuthAdapter: MSAuthAdapter = resolve()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .onAppear {
+                    msAuthAdapter.setupMSAuthentication()
+                    msAuthAdapter.loadCurrentAccount()
+                }
+                .onChange(of: scenePhase) { scenePhase in
+                    if scenePhase == .active {
+                        msAuthAdapter.loadCurrentAccount()
+                    }
+                }
+                .onOpenURL { url in
+                    msAuthAdapter.openUrl(url: url)
+                }
+                .environmentObject(msAuthState)
+                .environmentObject(DataModels())
+            }
+    }
+}
